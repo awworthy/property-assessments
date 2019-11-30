787472f5ca8196cd420a535b4f4da210e904c7bc
odlands <odlands@mymacewan.ca>
2019-11-30 10:49:11 -0700

    Added more map functionality
    Transferred changes from Tab3 class to MapTab class
    and deleted Tab3

--- src/ca/macewan/c305/PropertyTable.java
+++ src/ca/macewan/c305/PropertyTable.java
@@ -80,8 +80,8 @@ public class PropertyTable extends Application {
         tab1.setContent(borderPane);
         MapTab map =  new MapTab();
         tab2.setContent(map.start(propertyAssessments));
-        Tab3 map2 = new Tab3();
-        tab3.setContent(map2.start(propertyAssessments));
+//        Tab3 map2 = new Tab3();
+//        tab3.setContent(map2.start(propertyAssessments));
 
         Scene scene = new Scene(tabPane, 1200, 600);
         primaryStage.setScene(scene);
